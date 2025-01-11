f8e951a43211b28c1f091ac7e889cda8c4c2e9c4
Podystal-byte <istrizancev@gmail.com>
2025-01-11 22:48:19 +0300

    Create Classes

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,6 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
-        ArrayList<String> subTaskList = new ArrayList<>();
-        SubTask subTask = new SubTask(subTaskList);
-        HashMap<String, SubTask> epicMap = new HashMap<>();
-        epicMap.put("Уехать за границу", subTask);
 
-        Epic epic = new Epic(epicMap);
-        subTaskList.add("Побриться");
-        subTaskList.add("Помыться");
-        subTaskList.add("Убраться");
-        subTaskList.add("Подстричься");
-        epic.printAllSubTaskOnEpic("Уехать за границу");
     }
 }
